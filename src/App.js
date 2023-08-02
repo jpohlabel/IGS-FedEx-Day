683d2c4f47b0433c2cc5fb980fdc45960b2a2852
jpohlabel <jpohlabel@LIGS9723>
2023-08-02 11:06:41 -0400

    wheel stuff

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import ChoiceInput from './TextInput/TextInput.js';
 
 
 export default class App extends Component {
-    static displayName = 'SPIN THE WHEEL!!!';
-
+    static displayName = 'IGS WHEEL OF ENERGY';
     render() {
         return (
-            <div>
+            <div align='center'>
                 <Header></Header>
-                <h1 id="tabelLabel" >SPIN THE WHEEL</h1>
-                <p>This component SPINS THE WHEEL!!</p>
                 <Wheel></Wheel>
                 <ChoiceInput />
             </div>
